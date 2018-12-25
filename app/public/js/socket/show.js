ca26052d8dcd490b7044de15f903120f5da53f8c
Bùi Văn Xứng UIT <xungbv.uit@gmail.com>
2018-12-25 21:46:39 +0700

    edit rows

--- app/public/js/socket/show.js
+++ app/public/js/socket/show.js
@@ -1,3 +1,4 @@
+
 var socket;
 $(document).ready(function()
   {
@@ -25,7 +26,12 @@ $(document).ready(function()
     $('#n4Time').html("--/--/--");
     $('#n5Time').html("--/--/--");
     $('#n6Time').html("--/--/--");
-
+    $('#sF2').css({"background-color":"#99e699"});
+    $('#sF3').css({"background-color":"#99e699"});
+    $('#sF4').css({"background-color":"#99e699"});
+    $('#sF5').css({"background-color":"#99e699"});
+    $('#sF6').css({"background-color":"#99e699"});
+    $('#sF7').css({"background-color":"#99e699"});
 
     socket.emit('reciver_data_show', " ");
     socket.on('sending_data_show_Node_1', function (data) {
@@ -33,6 +39,28 @@ $(document).ready(function()
           $('#n1Temper').html(data[0].temperature);
           $('#n1Humid').html(data[0].humidity);
           $('#n1Time').html(convertTime(data[0].created_at));
+          switch(checkS(data[0].temperature,data[0].humidity)) {
+            case 0: {
+              $('#sF2').css({"background-color":"#009999"});
+              break;
+            }
+            case 1: {
+              $('#sF2').css({"background-color":"#99e699"});
+              break;
+            }
+            case 2: {
+              $('#sF2').css({"background-color":"#80ff80"});
+              break;
+            }
+            case 3: {
+              $('#sF2').css({"background-color":"#ffff66"});
+              break;
+            }
+            case 4: {
+              $('#sF2').css({"background-color":"#ff0000"});
+              break;
+            }
+          }
         }
     });
     socket.on('sending_data_show_Node_2', function (data) {
@@ -40,6 +68,28 @@ $(document).ready(function()
           $('#n2Temper').html(data[0].temperature);
           $('#n2Humid').html(data[0].humidity);
           $('#n2Time').html(convertTime(data[0].created_at));
+          switch(checkS(data[0].temperature,data[0].humidity)) {
+            case 0: {
+              $('#sF3').css({"background-color":"#009999"});
+              break;
+            }
+            case 1: {
+              $('#sF3').css({"background-color":"#99e699"});
+              break;
+            }
+            case 2: {
+              $('#sF3').css({"background-color":"#80ff80"});
+              break;
+            }
+            case 3: {
+              $('#sF3').css({"background-color":"#ffff66"});
+              break;
+            }
+            case 4: {
+              $('#sF3').css({"background-color":"#ff0000"});
+              break;
+            }
+          }
         }
     });
     socket.on('sending_data_show_Node_3', function (data) {
@@ -47,6 +97,28 @@ $(document).ready(function()
           $('#n3Temper').html(data[0].temperature);
           $('#n3Humid').html(data[0].humidity);
           $('#n3Time').html(convertTime(data[0].created_at));
+          switch(checkS(data[0].temperature,data[0].humidity)) {
+            case 0: {
+              $('#sF4').css({"background-color":"#009999"});
+              break;
+            }
+            case 1: {
+              $('#sF4').css({"background-color":"#99e699"});
+              break;
+            }
+            case 2: {
+              $('#sF4').css({"background-color":"#80ff80"});
+              break;
+            }
+            case 3: {
+              $('#sF4').css({"background-color":"#ffff66"});
+              break;
+            }
+            case 4: {
+              $('#sF4').css({"background-color":"#ff0000"});
+              break;
+            }
+          }
         }
     });
     socket.on('sending_data_show_Node_4', function (data) {
@@ -54,6 +126,28 @@ $(document).ready(function()
           $('#n4Temper').html(data[0].temperature);
           $('#n4Humid').html(data[0].humidity);
           $('#n4Time').html(convertTime(data[0].created_at));
+          switch(checkS(data[0].temperature,data[0].humidity)) {
+            case 0: {
+              $('#sF5').css({"background-color":"#009999"});
+              break;
+            }
+            case 1: {
+              $('#sF5').css({"background-color":"#99e699"});
+              break;
+            }
+            case 2: {
+              $('#sF5').css({"background-color":"#80ff80"});
+              break;
+            }
+            case 3: {
+              $('#sF5').css({"background-color":"#ffff66"});
+              break;
+            }
+            case 4: {
+              $('#sF5').css({"background-color":"#ff0000"});
+              break;
+            }
+          }
         }
     });
     socket.on('sending_data_show_Node_5', function (data) {
@@ -61,6 +155,28 @@ $(document).ready(function()
           $('#n5Temper').html(data[0].temperature);
           $('#n5Humid').html(data[0].humidity);
           $('#n5Time').html(convertTime(data[0].created_at));
+          switch(checkS(data[0].temperature,data[0].humidity)) {
+            case 0: {
+              $('#sF6').css({"background-color":"#009999"});
+              break;
+            }
+            case 1: {
+              $('#sF6').css({"background-color":"#99e699"});
+              break;
+            }
+            case 2: {
+              $('#sF6').css({"background-color":"#80ff80"});
+              break;
+            }
+            case 3: {
+              $('#sF6').css({"background-color":"#ffff66"});
+              break;
+            }
+            case 4: {
+              $('#sF6').css({"background-color":"#ff0000"});
+              break;
+            }
+          }
         }
     });
     socket.on('sending_data_show_Node_6', function (data) {
@@ -68,22 +184,61 @@ $(document).ready(function()
           $('#n6Temper').html(data[0].temperature);
           $('#n6Humid').html(data[0].humidity);
           $('#n6Time').html(convertTime(data[0].created_at));
+          switch(checkS(data[0].temperature,data[0].humidity)) {
+            case 0: {
+              $('#sF7').css({"background-color":"#009999"});
+              break;
+            }
+            case 1: {
+              $('#sF7').css({"background-color":"#99e699"});
+              break;
+            }
+            case 2: {
+              $('#sF7').css({"background-color":"#80ff80"});
+              break;
+            }
+            case 3: {
+              $('#sF7').css({"background-color":"#ffff66"});
+              break;
+            }
+            case 4: {
+              $('#sF7').css({"background-color":"#ff0000"});
+              break;
+            }
+          }
       }
     });
   })
+  function checkS(tm,hm) {
+    if (tm*1 <= 20 ) {
+      return 0;
+    }
+    if (tm*1 > 20 && tm*1 <= 27 ) {
+      return 1;
+    }
+    if (tm*1 > 27  && tm*1 <= 32) {
+      return 2;
+    }
+    if (tm*1 > 32 && tm*1 <= 36 ) {
+      return 3;
+    }
+    if (tm*1 > 36 && tm*1 <= 42 ) {
+      return 4;
+    }
+  }
   function convertTime(time) {
-    var t,d,s,tS,tM,tH,dD,dM,dY;
+    var t,d,s,tS,tM,tH,dD,dM;
     s = time.split("T");
     t = s[1].split(":");
     d = s[0].split("-");
     tS = t[2].split(".");
     tM = t[1];
-    tH = t[0]*1 + 2*1;
+    tH = t[0]*1;
     if (tH > 12) {
       tH = tH - 12;
     }
     dD = d[2];
     dM = d[1];
     dY = d[0];
-    return " "+ tH + ":" + tM + ":" + tS[0] + "  " + dD + "/" + dM;
+    return " "+ tH + ":" + tM + ":" + tS[0] + "  "+ "  " + dD + "/" + dM;
   }
